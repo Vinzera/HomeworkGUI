8adb0971a85fbb69effc7e49b7f31914807f0261
Vinzera <man.555552011@gmail.com>
2023-08-20 21:54:25 +0700

    Added Calculate Project

---
+++ calculate project/calculate project/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculate_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            int Textbox1 = int.Parse(income.Text);
+            int Textbox2 = int.Parse(textbox2.Text);
+            int Textbox3 = int.Parse(textbox3.Text);
+            int Textbox4 = Textbox3 / ( Textbox1 - Textbox2);
+            textbox4.Text = Textbox4.ToString();
+        }
+
+        private void textbox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
